31aacebb7d01536c5d4eb71f780f39c2c1e7a81f
LANCE LUMBA <lumba.lancejason.m.5348@gmail.com>
2023-03-17 00:48:05 +0800

    Update Number 6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,7 @@ import { Http, Response } from'@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 import { Album } from './album';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
@@ -13,7 +14,14 @@ export class ProductService {
 
   private _albumUrl= "../assets/album.json";
 
+  private _productsUrl= "../assets/album.json";
+
   getAlbum(id: number): Observable<Album> {
-    return this._http.get(this._albumUrl).map(response => <Album>response.json());  }
+    return this._http.get(this._albumUrl).map(response => <Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
+  }
 
 }
