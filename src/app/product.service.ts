cdb7a3d693c88eb3f445fe6ed964419b3bb5e075
LANCE LUMBA <lumba.lancejason.m.5348@gmail.com>
2023-03-17 00:00:55 +0800

    Update Number 2.9

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -15,7 +15,10 @@ export class ProductService {
 
   getAlbum(id:number){
     return this._http.get(this._albumUrl).map((response)=>response.json());
-    return Observable<Album>
+
+  }
+  getAlbum(Observable<Album>){
+    
   }
 
 }
